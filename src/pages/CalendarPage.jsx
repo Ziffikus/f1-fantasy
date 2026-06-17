4df3ea1148935dfdff50688d1e3814ef970a0e0e
Ziffikus <stroblalexander91@gmail.com>
2026-06-17 22:03:17 +0200

    traks

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -449,7 +449,7 @@ export function PastRaceExpanded({ w }) {
 
       {/* Track + Circuit */}
       <div className="cal-sessions-track">
-        <TrackMap round={w.round} circuit={w.circuit} country={w.country} size="md" />
+        <TrackMap round={w.round} circuit={w.circuit} country={w.country} size="full" />
       </div>
       <div className="cal-sessions-footer">
         <span className="cal-circuit">
@@ -535,7 +535,7 @@ export default function CalendarPage() {
                 <CalSessionRow label="Rennen 🏁"  dateStr={w.race_start} />
               </div>
               <div className="cal-sessions-track">
-                <TrackMap round={w.round} circuit={w.circuit} country={w.country} size="md" />
+                <TrackMap round={w.round} circuit={w.circuit} country={w.country} size="full" />
               </div>
               <div className="cal-sessions-footer">
                 <span className="cal-circuit">
