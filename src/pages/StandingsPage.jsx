00005b743abfdf41b63aec6f6ae52b112c2446a5
Ziffikus <stroblalexander91@gmail.com>
2026-06-21 00:03:49 +0200

    test

--- src/pages/StandingsPage.jsx
+++ src/pages/StandingsPage.jsx
@@ -594,7 +594,7 @@ export default function StandingsPage() {
                 {/* Aufgeklappte Renn-Details */}
                 {expanded === player.profile_id && (
                   <tr key={`${player.profile_id}-detail`} className="standings-detail-row">
-                    <td colSpan={100}>
+                    <td colSpan={isLive ? 7 : 6}>
                       <div className="standings-race-grid">
                         {completedWeekends.map(w => {
                           const rp = matrix[player.profile_id]?.[w.round]
